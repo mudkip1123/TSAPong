f39a757dd99ebb280b844ed2f67afdea7b793853
Kyle Swartley <kyleswartley@gmail.com>
2013-03-08 11:07:49 -0500

    Added bouncing off walls

--- ballPhysics.py
+++ ballPhysics.py
@@ -10,3 +10,16 @@ class PhysicalBall(pyglet.sprite.Sprite):
 	def update(self, dt):
 		self.x += self.vX * dt
 		self.y += self.vY * dt
+		self.wraparound(dt)
+
+	def wraparound(self, dt):
+		x, y, vx, vy = self.x, self.y, self.vX, self.vY
+
+		if x < 0 and vx < 0:
+			self.vX *= -1
+		if x > 800 and vx > 0:
+			self.vX *= -1
+		if y > 600 and vy > 0:
+			self.vY *= -1
+		if y < 0 and vy < 0:
+			self.vY *= -1
--- main.py
+++ main.py
@@ -3,11 +3,9 @@ import pyglet
 import load
 import ballPhysics
 
-win = pyglet.window.Window()
+win = pyglet.window.Window(width=800, height=600)
 
 ball = ballPhysics.PhysicalBall(img=load.ball_sprite, x=300, y=300)
-ball.vX = 20
-
 
 def update(dt):
 	ball.update(dt)
