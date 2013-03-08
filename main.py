c9358b68aee6662ce3ff7a713a0b3fb684957bb3
Kyle Swartley <kyleswartley@gmail.com>
2013-03-08 10:47:42 -0500

    Added helper files to Git

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import pyglet
 
+import load
+
 win = pyglet.window.Window()
+
+ball = pyglet.sprite.Sprite(img=load.ball_sprite)
+ball.x = 300
 
 
 def update(dt):
-	pass
+	ball.x += 1
+
 
 @win.event
 def on_draw():
 	win.clear()
+	ball.draw()
 
 pyglet.clock.schedule_interval(update, 1 / 120.)
 pyglet.app.run()
