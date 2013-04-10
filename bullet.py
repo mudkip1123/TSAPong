bb2797513627550dabc8f4d42a22a9411010ff64
Kyle Swartley <kyleswartley@gmail.com>
2013-04-10 13:22:42 -0400

    Upped speed and decreased lifetime

--- bullet.py
+++ bullet.py
@@ -3,10 +3,10 @@ import lineObject
 
 
 class Bullet(lineObject.Thing):
-	def __init__(self, lifetime=400, **kwargs):
+	def __init__(self, lifetime=100, **kwargs):
 		super(Bullet, self).__init__(pointsX=[0, 1, -1], pointsY=[1, -1, -1], **kwargs)
 		self.lifetime = lifetime
-		self.vel = physics.addAcceleration(self.vel, self.rotation, 100)
+		self.vel = physics.addAcceleration(self.vel, self.rotation, 400)
 
 		self.scale = 3
 
