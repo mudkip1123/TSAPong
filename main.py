385083113702f7e66d8ab37240062045cd6907f2
Kyle Swartley <kyleswartley@gmail.com>
2013-04-10 14:28:46 -0400

    Remove old commented-out code

--- main.py
+++ main.py
@@ -3,12 +3,10 @@ from pyglet.window import key
 
 import playerShip
 import asteroid
-import physics
 
 win = pyglet.window.Window(width=800, height=600)
 ball = playerShip.Ship(x=300, y=300)
 a = asteroid.buildAsteroidField()
-#foll = objectFollower.Follower(target=ball, speed=80.0)
 keys = key.KeyStateHandler()
 win.push_handlers(keys)
 
@@ -56,7 +54,6 @@ def on_draw():
 	ball.draw()
 	for i in a:
 		i.draw()
-	#foll.draw()
 
 pyglet.clock.schedule_interval(update, 1. / 120.)
 pyglet.app.run()
